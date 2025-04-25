31a79d6c3f32fe41543b8038ba081be89b4ef0a5
johnpaul <dexcripter@gmail.com>
2025-04-25 10:50:25 +0100

    connecting to a databasa

---
+++ src/config/connect-db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export default (): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(process.env.DATABASE_URI as string)
+      .then((conn) => {
+        resolve(`MongoDB connected: ${conn.connection.host}`);
+      })
+      .catch((err) => {
+        reject(`MongoDB connection error: ${err}`);
+      });
+  });
+};
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,18 @@ import http from "http";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
+// configure the dotenv package
+dotenv.config();
+
 import app from "./app";
+import connectDb from "./config/connect-db";
 
 const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000, async () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+  connectDb().catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+});
